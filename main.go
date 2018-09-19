dedab4f9d284c3fa98a5161042d51948e86750fd
Dominik Süß <dominik@suess.wtf>
2018-09-19 11:10:08 +0200

    init

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gramework/gramework"
+)
+
+func main() {
+	app := gramework.New()
+	app.Use(app.CORSMiddleware())
+	app.GET("/", func(ctx *gramework.Context) error {
+		ctx.HTML()
+		_, err := ctx.WriteString(`<html><body style="background-color: red"></body></html>`)
+		return err
+	})
+	app.ListenAndServe()
+}
