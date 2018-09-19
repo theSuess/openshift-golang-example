e3e9aeb81b3c1ec4a6bc8bffc623092a3499a50f
Dominik Süß <dominik@suess.wtf>
2018-09-19 11:29:44 +0200

    Changed color

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	app.Use(app.CORSMiddleware())
 	app.GET("/", func(ctx *gramework.Context) error {
 		ctx.HTML()
-		_, err := ctx.WriteString(`<html><body style="background-color: red"></body></html>`)
+		_, err := ctx.WriteString(`<html><body style="background-color: green"></body></html>`)
 		return err
 	})
 	app.ListenAndServe(":8080")
